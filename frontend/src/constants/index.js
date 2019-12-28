844eb9067730de1656e9c833ba8cf346bd022ead
Isabela Neri dos Santos <isabelandss@gmail.com>
2019-12-28 11:06:24 -0300

    constants

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,14 @@
+export default {
+  iof: 0.0638,
+  tx: 0.0234,
+  vehicle: {
+    min: 3000.00,
+    max: 100000.00,
+    installments: [24, 36, 48],
+  },
+  building: {
+    min: 30000.00,
+    max: 4500000.00,
+    installments: [120, 180, 240],
+  }
+}
