9d2ab04b53de67422c5d6e28b33cd7b2e77315f2
Isabela Neri dos Santos <isabelandss@gmail.com>
2019-12-28 16:35:19 -0300

    correcao de lint

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -2,7 +2,7 @@ import constants from '../constants'
 
 const toCurrency = ({ value = 0, withSymbol = false, numDecimal = 2 } = {}) => {
   const defaultOptions = { minimumFractionDigits: 2, maximumFractionDigits: numDecimal }
-  const symbolOptions = withSymbol ? { style: 'currency', currency: 'BRL' } : {} 
+  const symbolOptions = withSymbol ? { style: 'currency', currency: 'BRL' } : {}
   return Number(value).toLocaleString('pt-BR', { ...defaultOptions, ...symbolOptions })
 }
 
