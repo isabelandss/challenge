e8b991d824ad661bf39d9dc7787fffa02926e574
Isabela Neri dos Santos <isabelandss@gmail.com>
2019-12-28 13:08:56 -0300

    testes unitarios de toCurrency

---
+++ frontend/src/utils/index.spec.js
@@ -0,0 +1,31 @@
+import { toCurrency } from '.'
+
+describe('METHOD: toCurrency', () => {
+  test('should be a function', () => {
+    expect(typeof toCurrency).toBe('function')
+  })
+
+  test('should return default value', () => {
+    const expected = '0.00'
+    const received = toCurrency()
+    expect(received).toBe(expected)
+  })
+
+  test('should return formated value', () => {
+    const expected = '1,000.00'
+    const received = toCurrency({ value: 1000 })
+    expect(received).toBe(expected)
+  })
+
+  test('should return formated value with symbol', () => {
+    const expected = 'R$1,000.00'
+    const received = toCurrency({ value: 1000, withSymbol: true })
+    expect(received).toBe(expected)
+  })
+
+  test('should return formated value with more decimals', () => {
+    const expected = '1,000.1235'
+    const received = toCurrency({ value: 1000.1234567, numDecimal: 4 })
+    expect(received).toBe(expected)
+  })
+})
