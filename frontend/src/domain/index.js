34042629496a8c13a44f6e354364439d627b63d5
Isabela Neri dos Santos <isabelandss@gmail.com>
2019-12-28 13:50:38 -0300

    depara para confirmacao

--- frontend/src/domain/index.js
+++ frontend/src/domain/index.js
@@ -1,10 +1,10 @@
 import constants from '../constants'
 
-const totalValue = ({ installments = 0, loanAmount = 0 } = {}) => (constants.iof + constants.tax + (installments / 1000) + 1) * loanAmount
+const getTotalValue = ({ installments = 0, loanAmount = 0 } = {}) => (constants.iof + constants.tax + (installments / 1000) + 1) * loanAmount
 
-const installmentValue = ({ installments = 0, totalValue = 0 } = {}) => totalValue / installments || 0
+const getInstallmentValue = ({ installments = 0, totalValue = 0 } = {}) => totalValue / installments || 0
 
 export {
-  totalValue,
-  installmentValue,
+  getTotalValue,
+  getInstallmentValue,
 }
