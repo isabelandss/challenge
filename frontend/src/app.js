953f3b6f7e7ed28483460ff19eda7a5885435106
Isabela Neri dos Santos <isabelandss@gmail.com>
2019-12-30 09:46:13 -0300

    exportando funcoes que manipulam o dom

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,31 +1,31 @@
 import './styles/index.css'
 
 import { checkFormValidity, getFormValues, toCurrency, getElementsObjectByID } from './utils'
-import { loanService } from './services'
 import { getTotalValue, getInstallmentValue } from './domain'
+import { loanService } from './services'
 import constants from './constants'
+
+export const handleChangeForm = elements => {
+  const totalValue = getTotalValue({ installments: elements.installments.value, loanAmount: elements.loanvalue.value })
+  const installmentValue = getInstallmentValue({ installments: elements.installments.value, totalValue })
+  elements.totalvalue.innerHTML = toCurrency({ value: totalValue })
+  elements.installmentvalue.innerHTML = toCurrency({ value: installmentValue })
+  elements.taxvalue.innerHTML = `${toCurrency({ value: constants.tax * 100 })}%`
+}
+
+export const handleChangeWarrantyType = (elements, event) => {
+  elements.loanvalue.value = constants[event.target.value].min
+  elements.loanvaluerange.min = constants[event.target.value].min
+  elements.loanvaluerange.value = constants[event.target.value].min
+  elements.loanvaluerange.max = constants[event.target.value].max
+  elements.rangestart.innerHTML = toCurrency({ value: constants[event.target.value].min })
+  elements.rangeend.innerHTML = toCurrency({ value: constants[event.target.value].max })
+  elements.installments.innerHTML = constants[event.target.value].installments.map(i => `<option value=${i}>${i}</option>`).join('')
+}
 
 export const CreditasChallenge = (() => {
   const formElement = document.getElementById('form')
   const elements = getElementsObjectByID(document.querySelectorAll('form *'))
-
-  const handleChangeForm = () => {
-    const totalValue = getTotalValue({ installments: elements.installments.value, loanAmount: elements.loanvalue.value })
-    const installmentValue = getInstallmentValue({ installments: elements.installments.value, totalValue })
-    elements.totalvalue.innerHTML = toCurrency({ value: totalValue })
-    elements.installmentvalue.innerHTML = toCurrency({ value: installmentValue })
-    elements.taxvalue.innerHTML = `${toCurrency({ value: constants.tax * 100 })}%`
-  }
-
-  const handleChangeWarrantyType = event => {
-    elements.loanvalue.value = constants[event.target.value].min
-    elements.loanvaluerange.min = constants[event.target.value].min
-    elements.loanvaluerange.value = constants[event.target.value].min
-    elements.loanvaluerange.max = constants[event.target.value].max
-    elements.rangeend.innerHTML = toCurrency({ value: constants[event.target.value].max })
-    elements.rangestart.innerHTML = toCurrency({ value: constants[event.target.value].min })
-    elements.installments.innerHTML = constants[event.target.value].installments.map(i => `<option value=${i}>${i}</option>`).join('')
-  }
 
   const submitForm = (event, formElement) => {
     event.preventDefault()
@@ -43,12 +43,12 @@ export const CreditasChallenge = (() => {
     elements.warrantyvalue.addEventListener('input', event => (elements.warrantyvaluerange.value = event.target.value))
     elements.loanvalue.addEventListener('input', event => (elements.loanvaluerange.value = event.target.value))
 
-    formElement.addEventListener('change', handleChangeForm)
+    formElement.addEventListener('change', () => handleChangeForm(elements))
     formElement.addEventListener('submit', event => submitForm(event, formElement))
 
-    elements.warrantytype.addEventListener('change', handleChangeWarrantyType)
+    elements.warrantytype.addEventListener('change', event => handleChangeWarrantyType(elements, event))
 
-    handleChangeForm()
+    handleChangeForm(elements)
   }
 
   return {
